df2a3f98ed1d97e79b08c36143da549dca1dc297
Joseph Martinsen <joseph@martinsen.com>
2018-04-05 02:08:30 -0500

    Added node path to path

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,6 @@
 
 cd /home/ubuntu/express-api
 git pull
+export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v9.7.1/bin
 /home/ubuntu/.nvm/versions/node/v9.7.1/bin/npm run build
 sudo systemctl restart express-template
