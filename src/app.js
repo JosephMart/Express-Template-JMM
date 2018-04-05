c43849e9b6f7dbea5bf3d7a6c2475bbf138c5199
Joseph Martinsen <joseph@martinsen.com>
2018-04-04 23:16:47 -0500

    Update path to include /api/

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Routes
-app.use('/', routes);
+app.use('/api/', routes);
 
 // Catch 404 and forward to error handler
 app.use((req, res, next) => {
